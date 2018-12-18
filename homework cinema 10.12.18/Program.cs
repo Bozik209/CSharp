31a7ace7c97709aaf19b6f9cebf0cb55fbc221ad
boaz209 <44434303+boaz209@users.noreply.github.com>
2018-12-18 09:17:31 +0200

    Add files via upload

---
+++ homework cinema 10.12.18/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework_10._12._18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            cinema[] cinemaFood=new cinema[5];
+            cinemaFood[0] = new cinema("pop", 15, true, "osem", 2018);
+            cinemaFood[1] = new cinema("chocolate", 10, true, "para", 2017);
+            cinemaFood[2] = new cinema("hamburger", 150, false, "bbb", 2018);
+            cinemaFood[3] = new cinema("Fries", 30, true, "doritos", 2018);
+            cinemaFood[4] = new cinema("cola", 15, true, "cocacola", 2018);
+
+
+            for (int i = 0; i < cinemaFood.Length; i++)
+            {
+            Console.WriteLine(cinemaFood[i].GetDescription());
+            }
+        }
+    }
+}
---
+++ homework cinema 10.12.18/cinema.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework_10._12._18
+{
+    class cinema
+    {
+        private string product;
+        public string Product
+        {
+            get { return product; }
+            set { product = value; }
+        }
+
+        private int cost;
+        public int Cost
+        {
+            get { return cost; }
+            set { cost = value; }
+        }
+
+        private bool kosher;
+        public bool Kosher
+        {
+            get { return kosher; }
+            set { kosher = value; }
+        }
+
+        private string manufacturer;
+        public string Manufacturer
+        {
+            get { return manufacturer; }
+            set { manufacturer = value; }
+        }
+
+        private int expiration;
+        public int Expiration
+        {
+            get { return  expiration; }
+            set{
+                if (value>=2018){expiration = value; }
+                }
+        }
+
+
+        public cinema(string product,int cost,bool kosher, string manufacturer,int expiration)
+        {
+            Product = product;
+            Cost = cost;
+            Kosher = kosher;
+            Manufacturer = manufacturer;
+            Expiration = expiration;
+        }
+        public string GetDescription()
+        {
+            Console.WriteLine("-------------------------");
+            string rec = $"product name:{product}\n" +
+                $"cost:{cost}\n";
+            if (kosher)
+                rec += $"kosher: yes\n";
+            else
+                rec += $"kosher: no\n";
+            rec += $"Manufacturer: {manufacturer}\n";
+            if (expiration!=0)
+                rec += $"expiration: {expiration}\n";
+            else
+                rec += $"expiration at {expiration}\n";
+            return rec;
+        }
+    }
+}
